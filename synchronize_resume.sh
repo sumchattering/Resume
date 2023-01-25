e183a048947ef1ff92f8f01ba17161f1afca22af
Sumeru Chatterjee <sumeru.chatterjee@me.com>
2023-01-25 17:37:06 +0100

    Multiple files

--- synchronize_resume.sh
+++ synchronize_resume.sh
@@ -1,5 +1,7 @@
 # Shell script to synchronize resume from overleaf to github to Documents directory
 
+echo "Time is $(date)"
+
 echo "Syncing Resume from github releases to documents directory..."
 
 # Download file from github releases and save to Documents directory
@@ -10,3 +12,5 @@ curl -L  -o "$HOME/Documents/Resume/Resume.pdf" "$RESUME_URL" > /dev/null 2>&1
 IOS_RESUME_URL="https://github.com/sumchattering/Resume/releases/download/release/resume-ios.pdf"
 mkdir -p ~/Documents/Resume/ios
 curl -L -o $HOME/Documents/Resume/ios/Resume.pdf "$IOS_RESUME_URL" > /dev/null 2>&1
+
+echo "Done."
